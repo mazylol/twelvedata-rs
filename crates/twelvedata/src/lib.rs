d1c86b703984d76e90112d0bc9d4b8f067404737
mazylol <landonp95@gmail.com>
2023-06-07 00:54:52 -0400

    formatted

--- crates/twelvedata/src/lib.rs
+++ crates/twelvedata/src/lib.rs
@@ -1,17 +1,17 @@
 //! # Twelve Data API Client
-//! 
+//!
 //! This crate provides a client for the [Twelve Data API](https://twelvedata.com/docs).
-//! 
+//!
 //! The Twelve Data API provides financial market data for developers.
-//! 
+//!
 //! ## Quick Start
-//! 
+//!
 //! - First subscribe to the [Twelve Data API](https://rapidapi.com/twelvedata/api/twelve-data1).
 //! - Then create a new client with your API key.
-//! 
+//!
 //! ```rust
 //! use twelvedata::Client;
-//! 
+//!
 //! #[tokio::main]
 //! async fn main() {
 //!     let client = Client::new("TOKEN_HERE");
@@ -21,7 +21,7 @@
 //! ```
 //! - Run the program. It should return the current stock price of AAPL.
 //! - Now you can start using the client to get data from the Twelve Data API.
-//! 
+//!
 //! ## Notes
 //! - You need an async runtime to use this crate. I recommend [tokio](https://tokio.rs/) (as used above).
 //! - This crate is not yet complete. I will be adding more endpoints in the future.
@@ -33,7 +33,7 @@ pub mod core;
 pub mod reference;
 
 /// Client for the Twelve Data API
-/// 
+///
 /// # Example
 /// ```rust
 /// let client = Client::new("TOKEN_HERE");
