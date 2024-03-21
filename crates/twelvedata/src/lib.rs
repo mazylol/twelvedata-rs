c286ee6b4c287310f302adb9f34aa167718cfcbd
Landon <landonp95@gmail.com>
2024-03-20 21:20:10 -0400

    crate doc is now up to date

--- crates/twelvedata/src/lib.rs
+++ crates/twelvedata/src/lib.rs
@@ -1,19 +1,37 @@
-//! # Twelve Data API Client
+//! # Twelve Data API Wrapper
 //!
-//! This crate provides a client for the [Twelve Data API](https://twelvedata.com/docs).
+//! This crate provides a wrappers for the [Twelve Data API](https://twelvedata.com/docs).
 //!
 //! The Twelve Data API provides financial market data for developers.
 //!
 //! ## Quick Start
 //!
-//! - First subscribe to the [Twelve Data API](https://rapidapi.com/twelvedata/api/twelve-data1).
+//! - First subscribe to the [Twelve Data API](https://twelvedata.com). You can use the free tier to get started. Or don't, most of the endpoints in reference do not require an API key.
 //! - Then create a new client with your API key.
-//! - Now you can start using the wrapper to get data from the Twelve Data API.
+//! - Now you can start using the wrappers to get data from the Twelve Data API.
+//!
+//! ```rust
+//! use twelvedata::core;
+//!
+//! #[tokio::main]
+//! async fn main() {
+//!     let end_of_day = core::EndOfDay::builder()
+//!         .symbol("AAPL")
+//!         .apikey("your_api_key")
+//!         .execute()
+//!         .await;
+//! 
+//!     match end_of_day {
+//!         Ok(eod) => println!("{:?}", eod),
+//!         Err(e) => eprintln!("Error: {}", e),
+//!     }
+//! }
+//! ```
 //!
 //! ## Notes
 //! - You need an async runtime to use this crate. I recommend [tokio](https://tokio.rs/). It is actually what to run async tests.
 //! - This crate is not yet complete. I will be adding more endpoints in the future.
-//! - Paid endpoints are not yet supported. But will be put behind a feature flag when they are. But this will be difficult as I don't have a paid account to test with.
+//! - Paid endpoints/features are not fully supported. They are in the library, but are not neccessarily guaranteed to work. This is because I do not have a paid subscription to test them.
 
 /// Core Twelve Data
 pub mod core;
@@ -21,17 +39,3 @@ pub mod core;
 mod internal;
 /// Reference Twelve Data
 pub mod reference;
-
-/// Client for the Twelve Data API, slowly being phased out
-pub struct Client {
-    pub api_key: String,
-}
-
-impl Client {
-    /// Create a new client
-    pub fn new(api_key: &str) -> Client {
-        Client {
-            api_key: api_key.to_string(),
-        }
-    }
-}
